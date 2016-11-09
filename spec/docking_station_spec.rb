fcb0409f143eecfdac057fc852039c60c1a8fe37
Chris <chrixs99@hotmail.com>
2016-11-09 11:44:21 +0000

    refactored docking_station_spec.rb

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,9 +2,16 @@ require './lib/docking_station.rb'
 require './lib/bike.rb'
 
 describe DockingStation do
-  it {is_expected.to respond_to :release_bike}
-  it "shows if bike is working" do
-    bike = subject.release_bike
-    expect(bike.working?).to be
+
+  describe 'should respond to' do
+    subject {docking_station = DockingStation.new}
+    it {is_expected.to respond_to :release_bike}
+  end
+
+  describe "gets bike and checks if it's working" do
+    subject {docking_station = DockingStation.new
+    bike = docking_station.release_bike
+    bike.working?}
+    it {is_expected.to be true}
   end
 end
